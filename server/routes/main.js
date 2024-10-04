605013a9b466cae67e980831986e7281f1a75486
gabrieltrudes <esquecimeunome00@hotmail.com>
2024-10-04 16:37:08 -0300

    barra de pesquisa funcional

--- server/routes/main.js
+++ server/routes/main.js
@@ -35,25 +35,62 @@ router.get('', async (req, res) => {
     }
 });
 
+/**
+ * GET
+ * Post :id
+ */
+router.get('/post/:id', async (req, res) => {
+    try {
+        let slug = req.params.id;
+
+        const data = await Post.findById({_id: slug});
+
+        const locals = {
+            title: data.title,
+            description: "Blog criado com NodeJS, Express e MongoDB."
+        }
+
+    res.render('post', { locals, data });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+/**
+ * POST
+ * searchTerm
+ */
+router.post('/search', async (req, res) => {
+    try {
+        const locals = {
+            title: "Search",
+            description: "Blog criado com NodeJS, Express e MongoDB."
+        }
+
+        let searchTerm = req.body.searchTerm;
+        const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9 ]/g, "")
+
+        const data = await Post.find({
+            $or: [
+                { title: { $regex: new RegExp(searchNoSpecialChar, 'i')}},
+                { body: { $regex: new RegExp(searchNoSpecialChar, 'i')}}
+            ]
+        });
+
+        res.render("search", {
+            data,
+            locals
+        });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+router.get('/about', (req, res) => {
+    res.render('about');
+});
+
 module.exports = router;
-
-// router.get('/about', (req, res) => {
-//     res.render('about');
-// });
-
-// router.get('', async (req, res) => {
-//     const locals = {
-//         title: "NodeJS Blog",
-//         description: "Blog criado com NodeJS, Express e MongoDB."
-//     }
-
-//     try {
-//         const data = await Post.find();
-//         res.render('index', { locals, data });
-//     } catch (error) {
-//         console.log(error);
-//     }
-// });
 
 // function insertPostData () {
 //   Post.insertMany([
