7462bfa2f20fd7203fc047ae940effcefbc590a3
gabrieltrudes <esquecimeunome00@hotmail.com>
2024-10-10 17:56:57 -0300

    criando dashboard e configurando requisições de acesso

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,8 +2,30 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const adminLayout = '../views/layouts/admin';
+const adminLayout = "../views/layouts/admin";
+const jwtSecret = process.env.JWT_SECRET;
+
+/**
+ * Check Login
+ */
+const authMiddleware = (req, res, next) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ message: "Não autorizado." });
+  }
+
+  try {
+    const decoded = jwt.verify(token, jwtSecret);
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Não autorizado." });
+  }
+};
 
 /**
  * GET
@@ -23,23 +45,64 @@ router.get("/admin", async (req, res) => {
 });
 
 /**
- * GET
+ * POST
  * Admin - Check Login
  */
 router.post("/admin", async (req, res) => {
   try {
     const { username, password } = req.body;
-    
-    if (req.body.username === 'admin' && req.body.password === 'password') {
-      res.send('Login realizado com sucesso.')
-    } else {
-      res.send('Nome ou senha incorretos.')
+
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(401).json({ message: "Dados incorretos." });
     }
-    
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Dados incorretos." });
+    }
+
+    const token = jwt.sign({ userId: user.id }, jwtSecret);
+    res.cookie("token", token, { httpOnly: true });
+
+    res.redirect("/dashboard");
   } catch (error) {
     console.log(error);
   }
 });
 
+/**
+ * GET
+ * Dashboard
+ */
+router.get("/dashboard", authMiddleware, async (req, res) => {
+  res.render("admin/dashboard");
+});
+
+/**
+ * POST
+ * Admin - Register
+ */
+router.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    try {
+      const user = await User.create({ username, password: hashedPassword });
+      res.status(201).json({ message: "Usuário criado.", user });
+    } catch (error) {
+      if (error.code === 11000) {
+        res.status(409).json({ message: "Usuário já existe." });
+      } else {
+        res.status(500).json({ message: "Internal server error." });
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = router;
